7ba2fba83a108c776414475a54243c224f92d29b
Elena P <e.g.perfilyeva@gmail.com>
2021-03-02 11:13:04 -0800

    Shuffle the Array solution

---
+++ leetcode/shuffle_the_array.py
@@ -0,0 +1,16 @@
+# # 1470 Shuffle the Array
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        res = []
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[n+i])
+        return res
