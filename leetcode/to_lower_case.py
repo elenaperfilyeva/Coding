ce83145aaeb79d190f52420021d4d58806043742
Elena P <e.g.perfilyeva@gmail.com>
2021-03-05 00:13:40 -0800

    To Lower Case solution

---
+++ leetcode/to_lower_case.py
@@ -0,0 +1,10 @@
+#709 To Lower Case
+# Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
