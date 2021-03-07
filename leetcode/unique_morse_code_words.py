f02395a3386bff396132fa1dbcac9eedfadd2067
Elena P <e.g.perfilyeva@gmail.com>
2021-03-07 15:50:25 -0800

    Unique Morse Code Words solution

---
+++ leetcode/unique_morse_code_words.py
@@ -0,0 +1,57 @@
+#804 Unique Morse Code Words
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes,
+# as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+#
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+#
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...",
+# "-","..-","...-",".--","-..-","-.--","--.."]
+# Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter.
+# For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-...").
+# We'll call such a concatenation, the transformation of a word.
+#
+# Return the number of different transformations among all words we have.
+
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse_code = {
+            'a': ".-",
+            'b': "-...",
+            'c': "-.-.",
+            'd': "-..",
+            'e': ".",
+            'f': "..-.",
+            'g': "--.",
+            'h': "....",
+            'i': "..",
+            'j': ".---",
+            'k': "-.-",
+            'l': ".-..",
+            'm': "--",
+            'n': "-.",
+            'o': "---",
+            'p': ".--.",
+            'q': "--.-",
+            'r': ".-.",
+            's': "...",
+            't': "-",
+            'u': "..-",
+            'v': "...-",
+            'w': ".--",
+            'x': "-..-",
+            'y': "-.--",
+            'z': "--.."
+        }
+
+        transformations = []
+        for w in words:
+            morse_word = []
+            list_w = list(w.lower())
+            for char in list_w:
+                morse_word.append(morse_code[char])
+            transformations.append(''.join(morse_word))
+        return (len(set(transformations)))
