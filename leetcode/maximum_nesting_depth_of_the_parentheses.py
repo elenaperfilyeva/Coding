046f169df109cd4b66f5127768cea5f36bb686a5
Elena P <e.g.perfilyeva@gmail.com>
2021-03-07 14:12:31 -0800

    Maximum Nesting Depth of the Parentheses solution

---
+++ leetcode/maximum_nesting_depth_of_the_parentheses.py
@@ -0,0 +1,35 @@
+#1614 Maximum Nesting Depth of the Parentheses
+# A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+#
+# It is an empty string "", or a single character not equal to "(" or ")",
+# It can be written as AB (A concatenated with B), where A and B are VPS's, or
+# It can be written as (A), where A is a VPS.
+# We can similarly define the nesting depth depth(S) of any VPS S as follows:
+#
+# depth("") = 0
+# depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+# depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+# depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+# For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+#
+# Given a VPS represented as string s, return the nesting depth of s.
+
+class Solution(object):
+    def maxDepth(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        depth_arr = []
+        depth = 0
+        for elem in s:
+            if elem == '(':
+                depth+=1
+                depth_arr.append(depth)
+            elif elem == ')':
+                depth-=1
+                depth_arr.append(depth)
+        if len(depth_arr)==0:
+            return 0
+        else:
+            return max(depth_arr)
