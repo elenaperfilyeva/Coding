fbec03dde335d63673e1834fc84b977040b216bd
Elena P <e.g.perfilyeva@gmail.com>
2021-03-28 17:08:33 -0700

    Delete Columns to Make Sorted solution

---
+++ leetcode/delete_columns_to_make_sorted.py
@@ -0,0 +1,25 @@
+#944 Delete Columns to Make Sorted
+# You are given an array of n strings strs, all of the same length.
+#
+# The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"]
+# can be arranged as:
+#
+# abc
+# bce
+# cae
+# You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed),
+# columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+#
+# Return the number of columns that you will delete.
+
+class Solution(object):
+    def minDeletionSize(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+        counter = 0
+        for i in range(len(strs[0])):
+            current_column=[s[i] for s in strs]
+            counter+=(current_column!=sorted(current_column))
+        return counter
