97f71bf2c40e867409f63cba643ce4a3cb74453f
Elena P <e.g.perfilyeva@gmail.com>
2021-04-09 13:34:49 -0700

    Transpose Matrix another solution

--- leetcode/transpose_matrix.py
+++ leetcode/transpose_matrix.py
@@ -4,7 +4,7 @@
 # The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
 
 class Solution(object):
-    def transpose(self, matrix):
+    def transpose1(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
@@ -15,6 +15,19 @@ class Solution(object):
         for i in range(rows):
             for j in range(cols):
                  transpose_matrix[j][i] = matrix[i][j]
+        return transpose_matrix
+
+    def transpose2(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        transpose_matrix = []
+        for j in range(len(matrix[0])):
+            transpose_row = []
+            for i in range(len(matrix)):
+                transpose_row.append(matrix[i][j])
+            transpose_matrix.append(transpose_row)
         return transpose_matrix
 
 
