dac97bde1bc4aae2e6f28753cb0d8a328bfc49ec
Elena P <e.g.perfilyeva@gmail.com>
2021-04-17 12:12:34 -0700

    Check if One String Swap Can Make Strings Equal solution

---
+++ leetcode/check_if_one_string_swap_can_make_strings_equal.py
@@ -0,0 +1,23 @@
+#1790 Check if One String Swap Can Make Strings Equal
+# You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices
+# in a string (not necessarily different) and swap the characters at these indices.
+#
+# Return true if it is possible to make both strings equal by performing at most one string swap on exactly one
+# of the strings. Otherwise, return false.
+
+class Solution(object):
+    def areAlmostEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if s1 == s2: return True
+        s1_diff = []
+        s2_diff = []
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                s1_diff.append(s1[i])
+                s2_diff.append(s2[i])
+            if len(s1_diff)>2: return False
+        return sorted(s1_diff)==sorted(s2_diff)
