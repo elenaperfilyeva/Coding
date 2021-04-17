f8578e6b8bc051e585e67b7dc87ba537503d93bc
Elena P <e.g.perfilyeva@gmail.com>
2021-04-17 14:12:59 -0700

    Add Strings solution

---
+++ leetcode/add_strings.py
@@ -0,0 +1,13 @@
+#415 Add Strings
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1)+int(num2))
+
+        # without using build-in - usint ord('0')
