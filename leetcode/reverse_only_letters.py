48d4ad2b91cc0705e12d0a21c145e808f5006e2b
Elena P <e.g.perfilyeva@gmail.com>
2021-04-20 23:49:12 -0700

    Reverse Only Letters solution

---
+++ leetcode/reverse_only_letters.py
@@ -0,0 +1,22 @@
+#917 Reverse Only Letters
+# Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place,
+# and all letters reverse their positions.
+
+class Solution(object):
+    def reverseOnlyLetters(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        list_S = list(S)
+        left, right = 0, len(list_S)-1
+        while left < right:
+            if list_S[left].isalpha():
+                if list_S[right].isalpha():
+                    list_S[left], list_S[right] = list_S[right], list_S[left]
+                    left, right = left+1, right-1
+                else:
+                    right -= 1
+            else:
+                left += 1
+        return ''.join(list_S)
