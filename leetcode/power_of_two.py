0d7e87ea645f6be7742c82191b22a95780d607cc
Elena P <e.g.perfilyeva@gmail.com>
2021-04-30 12:46:33 -0700

    Power of Two solution

---
+++ leetcode/power_of_two.py
@@ -0,0 +1,16 @@
+#231 Power of Two
+# Given an integer n, return true if it is a power of two. Otherwise, return false.
+#
+# An integer n is a power of two, if there exists an integer x such that n == 2^x.
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0: return False
+        while n > 1:
+            if n%2 == 0: n = n/2
+            else: return False
+        return True
